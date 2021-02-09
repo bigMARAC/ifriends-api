90ad502477370a1504b7b300515977296a52e087
bigMARAC <marcao.ia18@gmail.com>
2021-02-08 23:11:18 -0300

    Config

---
+++ src/config/db-example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: '', /* 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+    host: '', /* HOST */
+    username: '', /* USER */ 
+    password: '', /* PASSWORD */
+    database: '', /* NAME OF DATABASE */
+    define: {
+        timestamps: false
+    }
+}
