3e3a9717468626f625545d3e3d738a896979b507
bigMARAC <marcao.ia18@gmail.com>
2021-02-15 00:58:16 -0300

    Add express.static

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+const path = require('path')
 
 require('./database')
 
 const app = express()
 
+app.use(express.static(__dirname + '/tmp'));
 app.use(express.json())
 app.use(cors())
 app.use(routes)
