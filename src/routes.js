c7eef12b9be0e5e7b14313f75899b757b6634f6a
Marcos Emanuel <marcos.length@gmail.com>
2021-02-17 10:16:17 -0300

    adjustments on routes

--- src/routes.js
+++ src/routes.js
@@ -13,22 +13,21 @@ routes.post('/alunos', AlunosController.store) // criar um aluno
 routes.post('/login', AlunosController.auth) // autenticar um aluno
 
 // alunos - rotas autenticadas
+routes.post('/picture', Auth.check, multer(multerConfig).single("file"), AlunosController.photo) // fazer upload da foto de perfil do aluno
 routes.get('/me', Auth.check, AlunosController.me) // exibir o aluno que está autenticado
 routes.get('/alunos', Auth.check, AlunosController.index) // listar alunos
 routes.put('/alunos', Auth.check, AlunosController.update) // atualizar um aluno
 routes.delete('/alunos/:id', Auth.check, AlunosController.destroy) // apagar um aluno
 
-routes.post('/picture', Auth.check, multer(multerConfig).single("file"), AlunosController.photo)
-
 // materias - rotas abertas
+routes.post('/materias', MateriasController.store) // criar materia
 routes.get('/materias', MateriasController.index) // listar materias
 
 // materias - rotas autenticadas
-routes.post('/materias', Auth.check, MateriasController.store) // criar materia
 routes.put('/materias/:id', Auth.check, MateriasController.update) // atualizar uma materia
 routes.delete('/materias/:id', Auth.check, MateriasController.destroy) // apagar uma materia
 
-// match
+// match - rotas autenticadas
 routes.post('/match', Auth.check, LikesController.store) // dar like em outro aluno
 routes.get('/match', Auth.check, LikesController.index) // listar os matches de um aluno
 routes.delete('/match/:origem_id', Auth.check, LikesController.delete) // cancelar um match
